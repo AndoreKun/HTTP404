403b1b2db5c5a01c0847779429e9af30c5be4614
André Pereira <andrepereira180903@gmail.com>
2020-12-24 18:13:55 +0000

    Finalizado Página de Login

---
+++ logado.php
@@ -0,0 +1,34 @@
+<?php
+	//starta a sess�o
+    	session_start();
+	ob_start();
+	//resgata os valores das session em variaveis
+	$id_users = isset($_SESSION['id_users']) ? $_SESSION['id_users']: "";	
+	$nome_user = isset($_SESSION['nome']) ? $_SESSION['nome']: "";
+	$email_users = isset($_SESSION['email']) ? $_SESSION['email']: "";	
+	$pass_users = isset($_SESSION['pass']) ? $_SESSION['pass']: "";
+	$logado = isset($_SESSION['logado']) ? $_SESSION['logado']: "N";	
+	//verificamos se a var logado cont�m o valor (S) OU (N), se conter N quer dizer que a pessoa n�o fez o login corretamente
+	//que no caso satisfar� nossa condi��o no if e a pessoa sera redirecionada para a tela de login novamente
+	if ($logado == "N" && $id_users == ""){	    
+		echo  "<script type='text/javascript'>
+					location.href='login.php'
+				</script>";	
+		exit();
+	}
+?>
+<!DOCTYPE HTML>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Sistema Login</title>
+</head>
+<body>
+	<center>
+		<article>
+			<h1><?php echo $nome_user;?> voc&ecirc; est&aacute; logado...</h1>
+			<a href="logout.php"><input type="button" value="Sair"/></a>
+		</article>
+	</center>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,104 @@
+<!DOCTYPE HTML>
+<html class="no-js" lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>HTTP 404</title>
+    <meta name="description" content="HTTP 404 - O melhor site de vendas de veículos">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    
+    <!-- all css here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/chosen.min.css">
+    <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/icofont.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/bundle.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="breadcrumb-area pt-255 pb-170" style="background-image: url(assets/img/banner/banner-4.jpg)">
+            <div class="container-fluid">
+                <div class="breadcrumb-content text-center">
+                    <h2>login</h2>
+                    <ul>
+                        <li>
+                            <a href="index.html">home</a>
+                        </li>
+                        <li>login de admin</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="login-register-area ptb-130">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 ml-auto mr-auto">
+                        <div class="login-register-wrapper">
+                            <div class="login-register-tab-list nav">
+                                <h2> Login </h2>
+                            </div>
+                            <div class="tab-content">
+                                <div id="lg1" class="tab-pane active">
+                                    <div class="login-form-container">
+                                        <div class="login-form">
+                                            <article>
+                                                <form name="form_pesquisa" id="form_pesquisa" method="post" action="">
+                                                    <div id="login-box-name">Email:</div>
+                            <div id="login-box-field">
+                                <input name="email" class="form-login" title="Username" value="" size="30"/>
+                            </div>
+                            <div id="login-box-name">Password:</div>
+                            <div id="login-box-field">
+                                <input name="pass" type="password" class="form-login" title="Password" value="" size="30"/>
+                            </div>
+                            <button name="acao" class="btn-style cr-btn" type="submit" form="form_pesquisa" value="logar">Login</button>
+                                                </form>
+                                            </article>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+<?php
+$action = isset($_POST['acao']) ? trim($_POST['acao']) : '';
+	if(isset($action) && $action != ""){ 
+		
+		switch($action){
+			case 'logar':
+				//requerimos nossa classe de autenticação passando os valores dos inputs como parâmetros
+				require_once('class/Autentica.class.php');
+				//instancio a classse para podermos usar o método nela contida
+				$Autentica = new Autentica();
+				//setamos 
+				$Autentica->email	= $_POST['email'];
+				$Autentica->pass	= $_POST['pass'];
+				//chamamos nosso método						
+				if($Autentica->Validar_Usuario()){	
+				   echo  "<script type='text/javascript'>
+							location.href='logado.php'
+						</script>";
+				  }else{
+				   echo  "<script type='text/javascript'>
+                            alert('ATEN\u00c7\u00c4O, Login ou Senha inv\u00e1lidos...');location.href='login.php'
+						</script>"; 
+				  }
+			break;
+		}	
+	}
+?>
