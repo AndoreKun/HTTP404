568a6a10dfb802fe392572e652d18fd5e81525aa
André Pereira <andrepereira180903@gmail.com>
2020-12-26 14:22:07 +0000

    Atualizado sistema de login, agora cado cargo é redirecionado para uma certa página

--- logado.php
+++ logado.php
@@ -1,5 +1,5 @@
 <?php
-	//starta a sess�o
+	//starta a sessão
     	session_start();
 	ob_start();
 	//resgata os valores das session em variaveis
@@ -7,9 +7,10 @@
 	$nome_user = isset($_SESSION['nome']) ? $_SESSION['nome']: "";
 	$email_users = isset($_SESSION['email']) ? $_SESSION['email']: "";	
 	$pass_users = isset($_SESSION['pass']) ? $_SESSION['pass']: "";
-	$logado = isset($_SESSION['logado']) ? $_SESSION['logado']: "N";	
-	//verificamos se a var logado cont�m o valor (S) OU (N), se conter N quer dizer que a pessoa n�o fez o login corretamente
-	//que no caso satisfar� nossa condi��o no if e a pessoa sera redirecionada para a tela de login novamente
+	$logado = isset($_SESSION['logado']) ? $_SESSION['logado']: "N";
+	$cargo = isset($_SESSION['cargo']) ? $_SESSION['cargo']: "";
+	//verificamos se a var logado contém o valor (S) OU (N), se conter N quer dizer que a pessoa não fez o login corretamente
+	//que no caso satisfará nossa condição no if e a pessoa sera redirecionada para a tela de login novamente
 	if ($logado == "N" && $id_users == ""){	    
 		echo  "<script type='text/javascript'>
 					location.href='login.php'
@@ -27,9 +28,9 @@
 	<center>
 		<article>
 			<?php
-			switch ($id_users) {
-				case 1:
-					header("location: localhost/index.html");
+			switch ($cargo) {
+				case "admin":
+					header("location: admin.php");
 					break;
 				}
 			?>	
