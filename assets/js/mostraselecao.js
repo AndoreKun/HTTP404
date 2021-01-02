c31cdc347af2859b553e9591b7c05b16cf14e5d1
André Pereira <andrepereira180903@gmail.com>
2021-01-02 20:00:16 +0000

    Correção de erros nos javascripts e css

--- assets/js/mostraselecao.js
+++ assets/js/mostraselecao.js
@@ -1,4 +1,4 @@
-function admSelectCheck(nameSelect)
+function admSelectCheck(nameSelect, adicionarvalores)
 {
     console.log(nameSelect);
     if(nameSelect){
@@ -9,8 +9,28 @@ function admSelectCheck(nameSelect)
         else{
             document.getElementById("elemento").style.display = "none";
         }
+    } 
+    else {
+        document.getElementById("elemento").style.display = "none";
+    }    
+    if (adicionarvalores == true){
+        admOptionValue2 = document.getElementById("opcao2").value;
+        admOptionValue3 = document.getElementById("opcao3").value;
+        if(admOptionValue2 == nameSelect.value){
+            document.getElementById("elemento2").style.display = "block";
+        }
+        else{
+            document.getElementById("elemento2").style.display = "none";
+        }
+        if(admOptionValue3 == nameSelect.value){
+            document.getElementById("elemento3").style.display = "block";
+        }
+        else{
+            document.getElementById("elemento3").style.display = "none";
+        }
     }
     else{
-        document.getElementById("elemento").style.display = "none";
+    document.getElementById("elemento2").style.display = "none";
+    document.getElementById("elemento3").style.display = "none";
     }
 }
