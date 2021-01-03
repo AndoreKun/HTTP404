a0b66d82a117a6771818f29ae9937520042c7e85
André Pereira <andrepereira180903@gmail.com>
2021-01-03 02:39:23 +0000

    inserts.php agora também redireciona para realizar uploads

--- database/inserts_basedados.php
+++ database/inserts_basedados.php
@@ -6,19 +6,29 @@ try{
         echo "Failed to connect to MySQL: " . $e->getMessage();
         exit();
     }
-    // set the PDO error mode to exception
+    // Coloca o modo de erro de PDO para excecao
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // use exec() because no results are returned
+    // usar exec() logo que nao sao devolvidos resultados
     try{
     $conexao->exec($insert);
     $sucesso = TRUE;
+    if($adicionar_foto == TRUE){
+        include('upload.php');
+    }
     } catch(PDOException $e) {
         echo  "<br>" . $e->getMessage();
         $sucesso = FALSE;
     }
-    if($sucesso == TRUE){?>
-        <h5 style="color:green">Dados adicionados com sucesso!</h5>
-    <?php
+    if($sucesso == TRUE){
+        if($acao == 'insert'){
+            ?><h5 style="color:green">Dados adicionados com sucesso!</h5><?php
+        }
+        if($acao == 'delete'){
+            ?><h5 style="color:green">Dados deletados com sucesso!</h5><?php
+        }
+        if($acao == 'update'){
+            ?><h5 style="color:green">Dados atualizados com sucesso!</h5><?php
+        }
     }
     $conexao = null;
 ?>
