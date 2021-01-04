ace16ebff422cacd4446f40df595885e19f0e5ca
André Pereira <andrepereira180903@gmail.com>
2021-01-04 00:50:05 +0000

    JavaScripts Finalizados

--- assets/js/mostraselecao.js
+++ assets/js/mostraselecao.js
@@ -9,7 +9,7 @@ function admSelectCheck(nameSelect, adicionarvalores)
         else{
             document.getElementById("elemento").style.display = "none";
         }
-    } 
+    }
     else {
         document.getElementById("elemento").style.display = "none";
     }    
@@ -32,5 +32,77 @@ function admSelectCheck(nameSelect, adicionarvalores)
     else{
     document.getElementById("elemento2").style.display = "none";
     document.getElementById("elemento3").style.display = "none";
+    }
+}
+function admSelectCheck2(nameSelect, adicionarvalores)
+{
+    console.log(nameSelect);
+    if(nameSelect){
+        admOptionValue = document.getElementById("opcao4").value;
+        if(admOptionValue == nameSelect.value){
+            document.getElementById("elemento4").style.display = "block";
+        }
+        else{
+            document.getElementById("elemento4").style.display = "none";
+        }
+    } 
+    else {
+        document.getElementById("elemento4").style.display = "none";
+    }    
+    if (adicionarvalores == true){
+        admOptionValue2 = document.getElementById("opcao5").value;
+        admOptionValue3 = document.getElementById("opcao6").value;
+        if(admOptionValue2 == nameSelect.value){
+            document.getElementById("elemento5").style.display = "block";
+        }
+        else{
+            document.getElementById("elemento5").style.display = "none";
+        }
+        if(admOptionValue3 == nameSelect.value){
+            document.getElementById("elemento6").style.display = "block";
+        }
+        else{
+            document.getElementById("elemento6").style.display = "none";
+        }
+    }
+    else{
+    document.getElementById("elemento5").style.display = "none";
+    document.getElementById("elemento6").style.display = "none";
+    }
+}
+function admSelectCheck3(nameSelect, adicionarvalores)
+{
+    console.log(nameSelect);
+    if(nameSelect){
+        admOptionValue = document.getElementById("opcao7").value;
+        if(admOptionValue == nameSelect.value){
+            document.getElementById("elemento7").style.display = "block";
+        }
+        else{
+            document.getElementById("elemento7").style.display = "none";
+        }
+    } 
+    else {
+        document.getElementById("elemento7").style.display = "none";
+    }    
+    if (adicionarvalores == true){
+        admOptionValue2 = document.getElementById("opcao8").value;
+        admOptionValue3 = document.getElementById("opcao9").value;
+        if(admOptionValue2 == nameSelect.value){
+            document.getElementById("elemento8").style.display = "block";
+        }
+        else{
+            document.getElementById("elemento8").style.display = "none";
+        }
+        if(admOptionValue3 == nameSelect.value){
+            document.getElementById("elemento9").style.display = "block";
+        }
+        else{
+            document.getElementById("elemento9").style.display = "none";
+        }
+    }
+    else{
+    document.getElementById("elemento8").style.display = "none";
+    document.getElementById("elemento9").style.display = "none";
     }
 }
